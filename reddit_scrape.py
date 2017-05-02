514c0bbb895addd3b426da1d38871f03302dee08
Kelly Mack <knmack2@illinois.edu>
2017-05-01 23:47:46 -0500

    can get names and comments of subreddit

--- reddit_scrape.py
+++ reddit_scrape.py
@@ -4,5 +4,11 @@ reddit = praw.Reddit(client_id='2YtQ3LoT3ImDnQ',
                      client_secret='0AmQwHwHO2SByypfkmE6vh4MMsI',
                      user_agent='Excel Scraping')
 
-for submission in reddit.subreddit('excel').hot(limit=10):
+for submission in reddit.subreddit('excel').hot(limit=1):
     print(submission.title)
+#    submission.comments.replace_more(limit=10)
+#    for top_level in submission.comments:
+#        print(top_level.body)
+    all_comments = submission.comments.list()
+    for comment in all_comments:
+        print(comment.body)
